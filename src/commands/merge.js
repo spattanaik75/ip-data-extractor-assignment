72de8c6534dc9e8f3752593f7d8e4ce477db9dbf
Soumya Pattanaik <SoumyaP@discovery.co.za>
2020-01-23 10:14:01 +0200

    added basic code for validation, sort and merge

--- src/commands/merge.js
+++ src/commands/merge.js
@@ -1,26 +1,19 @@
-// this library is used for better CLI user experience
-const stdio = require('stdio');
-const readline = require('readline');
-
-
-const opts = stdio.getopt({
-    "file1": {description: "first file location", mandatory: true, args:1 },
-    "file2": {description: "first file location", mandatory: true, args:1 }
-});
-
+const fs = require('fs');
+const lineByLine = require('n-readlines');
 
 // could also use the library 
 // let isValidIp = require('is-ip');
 // isValidIp('192.168.0.1');
-function isValidIP(str) {
+const isValidIP = (str) => {
     const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)';
     const regex = new RegExp(`^${octet}\\.${octet}\\.${octet}\\.${octet}$`);
     return regex.test(str);
-  }
+};
+
 
 // alternatives are array.sort()
 // quick sort is chosen here for better average time complexity O(nlogn)
-function quick_Sort(origArray) {
+const quickSort = (origArray)=>{
 	if (origArray.length <= 1) { 
 		return origArray;
 	} else {
@@ -37,8 +30,40 @@ function quick_Sort(origArray) {
 				right.push(origArray[i]);
 			}
 		}
-		return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
+		return newArray.concat(quickSort(left), pivot, quickSort(right));
 	}
-}
+};
 
-console.log(process.argv);
+const mergeAndSort= (file) =>{
+	if(fs.existsSync(file)){
+		const liner = new lineByLine(file);
+		let line;
+		while (line = liner.next()){
+			const line_arr = line.toString().split(":");
+			if (line_arr.length >0){
+				const line_ip = line_arr[0].trim();
+				// TODO: add ip validation logic here
+				const line_digits = line.toString().split(":")[1].trim().split(",");
+				// TODO: add digits validation logic here
+				if (result.get(line_ip) !== undefined){	 
+					result.set(line_ip , quickSort(Array.from(new Set(result.get(line_ip).concat(line_digits)))));
+				}else{
+					result.set(line_ip, quickSort(Array.from(new Set(line_digits))));
+				}
+			}
+		}
+	}else{
+		console.log(`file ${file} does not exist`);
+	}
+};
+
+const result = new Map();
+
+const files = process.argv.slice(2);
+files.forEach(
+	mergeAndSort
+);
+
+result.forEach((value, key) =>{
+	console.log(`${key}: ${value}`);
+});
